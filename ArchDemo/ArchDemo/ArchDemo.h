7af5c561e817db94ed97ba14fad16cafba9cc5b5
arimura <kotaro_arimura@voyagegroup.com>
2021-05-17 21:46:42 +0900

    init

---
+++ ArchDemo/ArchDemo/ArchDemo.h
@@ -0,0 +1,18 @@
+//
+//  ArchDemo.h
+//  ArchDemo
+//
+//  Created by arimura on 2021/05/17.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ArchDemo.
+FOUNDATION_EXPORT double ArchDemoVersionNumber;
+
+//! Project version string for ArchDemo.
+FOUNDATION_EXPORT const unsigned char ArchDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ArchDemo/PublicHeader.h>
+
+
